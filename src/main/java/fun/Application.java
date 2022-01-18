977dab8bfcb9435e25caf4f374e687de9f2932f0
muslima <muslima@gmail.com>
2022-01-18 21:43:59 +0500

    Deploying spring boot java app

---
+++ src/main/java/fun/Application.java
@@ -0,0 +1,20 @@
+package fun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class Application {
+
+    @RequestMapping("/")
+    public String home () {
+      return "hey guys! hope you are enjoying this";
+    }
+
+    public static void main(String[] args) {
+      SpringApplication.run(Application.class, args);
+    }
+}
